a40ac3c3bd8bd4862cc873a6fed06c2bc721aaf6
Lorenzo Rutayisire <lorenzorutayisire@gmail.com>
2023-02-04 22:46:39 +0100

    Add new tables to tables.ts

--- migrations/tables.ts
+++ migrations/tables.ts
@@ -45,10 +45,30 @@ async function initTables() {
         court int references courts(id)
     );    
     
-        
+    CREATE TABLE IF NOT EXISTS accelerometer_data(
+        id int primary key AUTO_INCREMENT,
+        basket_id int not null,
+        accel_x float not null,
+        accel_y float not null,
+        accel_z float not null,
+        gyro_x float not null,
+        gyro_y float not null,
+        gyro_z float not null,
+        temperature float not null,
+        timestamp timestamp not null
+    );
     
+    CREATE TABLE IF NOT EXISTS basket_data(
+        id int primary key AUTO_INCREMENT,
+        basket_id int not null,
+        timestamp timestamp not null
+    );
     
-    
+    CREATE TABLE IF NOT EXISTS people_detected_data(
+        id int primary key AUTO_INCREMENT,
+        basket_id int not null,
+        timestamp timestamp not null
+    );
     `)
 }
 
