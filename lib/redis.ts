7e0010071b7510f516524e614f43f531490a15f3
Giulio Barabino <giuliobarabino99@gmail.com>
2023-02-10 15:33:21 +0100

    Added redis support
    Co-authored-by: Carmine Zaccagnino <carmine@carminezacc.com>
    Signed-off-by: Giulio Barabino <giuliobarabino99@gmail.com>

---
+++ lib/redis.ts
@@ -0,0 +1,19 @@
+import {createCLient} from 'redis';
+
+const client = createClient({
+    host: 'redis',
+    port: 6379,
+});
+
+export const setHasScored = async (game_id: number) => {
+    await client.set(`game:${game_id}:has_scored`, '1');
+}
+
+export const getHasScored = async (game_id: number): Promise<boolean> => {
+    let raw = await client.get(`game:${game_id}:has_scored`);
+    return raw == '1';
+}
+
+export const resetHasScored = async (game_id: number) => {
+    await client.del(`game:${game_id}:has_scored`);
+}
