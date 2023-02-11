a8254f6009b734fe605eecb4a0f2d38c2a80df39
Giulio Barabino <giuliobarabino99@gmail.com>
2023-02-11 15:21:15 +0100

    work in progress: mqtt client and db insertions
    Signed-off-by: Giulio Barabino <giuliobarabino99@gmail.com>

--- lib/Game.ts
+++ lib/Game.ts
@@ -27,6 +27,45 @@ class Game {
         );
     }
 
+    static async getByBasketId(basket_id: number): Promise<Game[]> {
+        let result =  await db.query("SELECT id, basket, score1, score2, created_at FROM simple_games WHERE basket=?", [basket_id]);
+        return result.results;
+    }
+
+    static async checkIfBasketHasGame(basket_id: number): Promise<boolean> {
+        let result =  await db.query("SELECT id, basket, score1, score2, created_at FROM simple_games WHERE basket=? AND score1=0 AND score2=0", [basket_id]);
+        return result.results.length > 0;
+    }
+
+    static async insertScoreData(basket_id: number): Promise<void> {
+        return await db.query(
+            'INSERT INTO score_data(basket) VALUES (?)',
+            [basket_id]
+        );
+    }
+
+    static async insertAccelerometerData(basket_id: number, acc_x: number, acc_y: number, acc_z: number, gyro_x: number, gyro_y: number, gyro_z: number, temperature: number): Promise<void> {
+        return await db.query(
+            'INSERT INTO accelerometer_data(basket, acc_x, acc_y, acc_z, gyro_x, gyro_y, gyro_z, temperature) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+            [basket_id, acc_x, acc_y, acc_z, gyro_x, gyro_y, gyro_z, temperature]
+        );
+    }
+
+    static async updateScore(basket_id: number, score1: number, score2: number): Promise<void> {
+        return await db.query(
+            'UPDATE simple_games SET score1=?, score2=? WHERE basket=?',
+            [score1, score2, basket_id]
+        );
+    }
+
+    static async insertPeopleDetected(basket_id: number): Promise<void> {
+        return await db.query(
+            'INSERT INTO people_detected(basket) VALUES (?)',
+            [basket_id]
+        );
+    }
+
+
     get schemaData() {
         return {
             id: this.id,
---
+++ lib/MQTTClient.ts
@@ -0,0 +1,116 @@
+import Game from './Game';
+import * as redis from './redis';
+
+const mqtt = require('mqtt');
+
+export default class MQTTClient{
+    client: any;
+
+    constructor(){
+        console.log("MQTTClient created");
+    }
+
+    init(): void{
+
+        const client = mqtt.connect('mqtt://'+process.env.BROKER_ADDRESS+":"+process.env.BROKER_PORT);
+        
+        client.on('connect', function () {
+            client.subscribe('#', function (err) {
+                if (!err) {
+                    console.log("MQTTClient connected");
+                }
+            })
+        })
+
+        client.on('message', (topic, message) => {
+            
+            console.log('Received message from topic:', topic);
+            message = JSON.parse(message.toString());
+
+            const basket_id = message.metadata[0];
+            const hasGame = Game.checkIfBasketHasGame(basket_id);
+
+            switch(message.type){
+                case "SCORE":
+                    this.on_score(message);
+                    break;
+                case "ACCELEROMETER":
+                    this.on_accelerometer(message);
+                    break;
+                case "CUSTOM_BUTTON":
+                    this.on_custom_button(message);
+                    break;
+                case "PEOPLE_DETECTED":
+                    this.on_people_detected(message);
+                    break;
+                default:
+                    console.log("Unknown message type");
+            }
+        })
+
+    }
+
+    on_score(message: any): void{
+        const basket_id = message.metadata[0];
+        console.log("SCORE: received from basket " + basket_id);
+    
+        Game.insertScoreData(basket_id);
+
+        // setta in redis che è stato fatto un canestro
+        redis.setHasScored(basket_id);
+    }
+
+    on_accelerometer(message: any): void{
+        const basket_id = message.metadata[0];
+        const acc_x = message.metadata[1];
+        const acc_y = message.metadata[2];
+        const acc_z = message.metadata[3];
+        const gyro_x = message.metadata[4];
+        const gyro_y = message.metadata[5];
+        const gyro_z = message.metadata[6];
+        const temp = message.metadata[7];
+        console.log("ACCELEROMETER: received from basket " + basket_id);
+
+        Game.insertAccelerometerData(basket_id, acc_x, acc_y, acc_z, gyro_x, gyro_y, gyro_z, temp);
+    }
+
+    on_custom_button(message: any): void{
+        const basket_id = message.metadata[0];
+        const button_id = message.metadata[1];
+
+        const game = Game.getByBasketId(basket_id);
+        const score1 = game[0].score1;
+        const score2 = game[0].score2;
+
+        // controlla in redis se è stato fatto un canestro,
+        // resetta a 0 in redis il flag che controlla se è stato fatto un canestro
+
+        redis.getHasScored(basket_id).then((hasScored: boolean) => {
+            
+            switch(button_id){
+                case 1:
+                    Game.updateScore(basket_id, score1 + 1, score2);
+                    break;
+                case 2:
+                    Game.updateScore(basket_id, score1, score2 + 1);
+                    break;
+                default:
+                    console.log("Unknown button id");
+            }
+
+        });
+
+        redis.resetHasScored(basket_id);
+
+        console.log("CUSTOM_BUTTON: received from basket " + basket_id + " button " + button_id)
+    }
+
+    on_people_detected(message: any): void{
+        const basket_id = message.metadata[0];
+        console.log("PEOPLE_DETECTED: received from basket " + basket_id);
+
+        Game.insertPeopleDetected(basket_id);
+    }
+
+}
+
